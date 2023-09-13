5b2b5dd897c893b7543e1e30d9671e2ab417011d
DmitryAnisimovJava <130083398+DmitryAnisimovJava@users.noreply.github.com>
2023-09-13 12:17:22 +0300

    Complete lambda task.

--- src/main/java/com/epam/mjc/InterfaceCreator.java
+++ src/main/java/com/epam/mjc/InterfaceCreator.java
@@ -1,8 +1,21 @@
 package com.epam.mjc;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 public class InterfaceCreator {
 
     public Operation<Integer> divideBy(Integer divider) {
-        throw new UnsupportedOperationException("You should implement this method.");
+    	
+    	Operation<Integer> divideOperation = x -> {
+    		List<Integer> returnList = new ArrayList<>();
+    		for (Iterator<Integer> iterator = x.iterator(); iterator.hasNext();) {
+				Integer integer = iterator.next();
+				returnList.add(integer / divider);
+			}
+    		return returnList;
+    	};
+    	return divideOperation;
     }
 }
